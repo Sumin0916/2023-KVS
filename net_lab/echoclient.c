e97a450b162ce82df1a409cd9d921a7877829ef3
Sumin0916 <gsmin2018@gmail.com>
2023-12-07 20:50:35 +0900

    ADD NET DATA

--- net_lab/echoclient.c
+++ net_lab/echoclient.c
@@ -8,8 +8,41 @@
 
 int main(int argc, char** argv) 
 {
-	return 0;
+   int clientfd, port;
+   struct sockaddr_in serveraddr;
+
+   char *host, buf[MAXLINE];
+
+   host=  argv[1];
+   port = atoi(argv[2]);
+
+   if ((clientfd = socket(AF_INET, SOCK_STREAM, 0))<0){
+      perror("socket");
+      exit(1);
+   }
+
+   serveraddr.sin_family = AF_INET;
+   serveraddr.sin_port = htons((unsigned short) port);
+   serveraddr.sin_addr.s_addr = inet_addr(host);
+   
+   if(connect(clientfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr))<0){
+      perror("Connect");
+      exit(1);
+   }
+
+   int rbytes, wbytes;
+   
+   while(fgets(buf, MAXLINE, stdin) != NULL){
+      wbytes = write(clientfd, buf, strlen(buf));
+      if (wbytes < strlen(buf))
+         printf("Failed to send message\n");
+      rbytes = read(clientfd ,buf,MAXLINE);
+      fputs(buf, stdout);
+   }
+   close(clientfd);
+   return 0;
+
+   return 0;
 
 }
-
 
